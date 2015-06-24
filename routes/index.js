e3c8964f6f1d1b65290d8a862142c22164611081
alexanderkatz <katzkode@gmail.com>
2015-06-24 10:47:46 -0400

    Outlined routes for new entry, blogroll, and post new entry.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET newpost. */
+router.get('/newentry', function(req, res, next) {
+  res.render('newentry-view', { title: 'New Post' });
+});
+
+/* GET blogroll. */
+router.get('/blogroll', function(req, res, next) {
+  res.render('blogroll-view', { title: 'Blog Roll' });
+});
+
+/* POST to entry. */
+router.post('/postentry', function(req, res, next) {
+});
+
 module.exports = router;

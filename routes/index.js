78f149bea3043a0eec28480e03b5cc3a4a740696
alexanderkatz <katzkode@gmail.com>
2015-06-24 13:21:58 -0400

    Final commit before switching to IB's machine

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', {
+		title: 'Express'
+	});
 });
 
 /* GET newpost. */
-router.get('/newentry', function(req, res, next) {
-  res.render('newentry-view', { title: 'New Post' });
+router.get('/newentry', function (req, res, next) {
+	res.render('newentry-view', {
+		title: 'New Post'
+	});
 });
 
-/* GET blogroll. */
-router.get('/blogroll', function(req, res, next) {
-  res.render('blogroll-view', { title: 'Blog Roll' });
+router.get('/blogroll', function (req, res, next) {
+	var db = req.db;
+	res.render('blogroll-view', {
+		title: 'All Posts'
+	});
 });
+
+
+
+//	var collection = db.get('entrycollection');
+//	collection.find({}, {}, function (e, docs) {
+
+//	})
+
+
 
 /* POST to entry. */
-router.post('/postentry', function(req, res, next) {
-});
+router.post('/postentry', function (req, res, next) {});
 
 module.exports = router;

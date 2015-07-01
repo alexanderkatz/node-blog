9050e90057480a3ee8ebb94c794698c5dcaf3309
alexanderkatz <katzkode@gmail.com>
2015-07-01 12:15:45 -0400

    Insert users to database and display information with AJAX

--- public/javascripts/usertable.js
+++ public/javascripts/usertable.js
@@ -8,6 +8,10 @@ $(document).ready(function(){
 
     // username link click
     $('#usertable tbody').on('click', 'td a.linkshowuser', showUserInfo);
+
+    // add userButton click
+    $('#btnAddUser').on('click', addUser);
+
 });
 
 // Functions =============================================================
@@ -24,7 +28,7 @@ function populateTable(){
         // and cells to the content string
         $.each(data, function(){
             tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.userName + '">' + this.userName + '</a></td>';
             tableContent += '<td>' + this.email + '</td>';
             tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
@@ -44,21 +48,71 @@ function showUserInfo(event) {
 
     // get index of oject based on id value
     var arrayPosition = userListData.map(function(arrayItem){
-        return arrayItem.username;
+        return arrayItem.userName;
     }).indexOf(thisUserName);
-
-
-
 
     // get our user object
     var thisUserObject = userListData[arrayPosition];
 
     // populate info box
-    $('#userInfoName').text(thisUserObject.fullname);
-    console.log(thisUserObject.fullname);
+    $('#userInfoName').text(thisUserObject.fullName);
+    console.log(thisUserObject.fullName);
     $('#userInfoAge').text(thisUserObject.age);
     console.log(thisUserObject.age);
     $('#userInfoGender').text(thisUserObject.gender);
     console.log(thisUserObject.gender);
     $('#userInfoLocation').text(thisUserObject.location);
+};
+
+/* addUser function */
+function addUser(event){
+    event.preventDefault();
+
+    // super basic validation, increase error count var if any fields are bland
+    var errorCount = 0;
+    $('#newUserForm input').each(function(index,val){
+        if ($(this).val() === ''){
+            errorCount++;
+        }
+    });
+    //console.log("errorCount: "+errorCount);
+
+    // check that errorCount is still at 0
+    if (errorCount === 0){
+        // if it is compile all user info into one object
+        // console.log("errorCount does equal 0");
+        var newUser = {
+            'userName':$('#newUserForm input#inputUserName').val(),
+            'email':$('#newUserForm input#inputUserEmail').val(),
+            'fullName':$('#newUserForm input#inputUserFullName').val(),
+            'age':$('#newUserForm input#inputUserAge').val(),
+            'location':$('#newUserForm input#inputUserLocation').val(),
+            'gender':$('#newUserForm input#inputUserGender').val()
+        }
+        // console.log("newUser: "+newUser.email);
+
+        // use AJAX to POST the object to our add user service
+        $.ajax({
+            url: '/users/insertuser',
+                type: 'POST',
+                data: newUser,
+        }).done(function(response) {
+            // check for successful blank response
+            if (response.msg === ''){
+                // clear the form inputs
+                $('#newUserForm input').val('');
+                //update the table
+                populateTable();
+            }
+            else{
+                // if something goes wrong alert error msg that our service returned
+                alert("error: "+response.msg);
+            }
+        });
+    }
+    else{
+        // if error count > 0, error out
+        alert("Please fill in all fields");
+        return false;
+    }
 };
--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,17 @@ router.get('/userlist', function(req,res){
     });
 });
 
+/* POST new user to db */
+router.post('/insertuser', function(req,res){
+	var db=req.db;
+	var collection = db.get('userlist');
+
+	collection.insert(req.body, function(err,result){
+		res.send(
+			(err===null) ? {msg: ''} : {msg: err}
+		);
+	});		
+});
+
+
 module.exports = router;

6cbd53bac84edf5aff55c7cce817685aab6894c4
ibrand <me@ilonabrand.com>
2015-07-03 11:33:00 -0400

    set up POST for /signup

--- routes/index.js
+++ routes/index.js
@@ -106,7 +106,12 @@ module.exports = function(app, passport){
 	});
 
 	// process the login form
-	//app.post('/signup', do all our passport stuff here);
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile',	// redirect to the secure profile section
+		failureRedirect : '/signup', 	// redirect back to signup page if there's an error
+		failureFlash : true		// allow flash messages
+	}));
+	
 	// =====================================
 	// PROFILE SECTION =====================
 	// =====================================

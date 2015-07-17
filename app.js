23f77230f04aec5d2e800d1a0cd90393857ceb0f
alexanderkatz <katzkode@gmail.com>
2015-07-17 18:39:23 -0400

    Cookies shared between subdomain and root

--- app.js
+++ app.js
@@ -41,11 +41,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 // required for passport
 app.use(session({
   secret:'hititwithacrayon',
-  domain: '.purplecrayon.me'  
+  cookie:{domain:'.purplecrayon.me'}
 }));
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Origin', req.headers.origin);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+//     next();
+// });
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash()); // use connect-flash for flash messages stored in session
